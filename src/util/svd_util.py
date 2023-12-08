60b8f11758840e965bab505c0a90f334cc09f15c
Sumit Sahoo <sumitsahoo1988@gmail.com>
2023-12-08 20:56:50 +0530

    Refactor generate_video_from_image method to handle exceptions

--- src/util/svd_util.py
+++ src/util/svd_util.py
@@ -8,19 +8,22 @@ class SVDUtil:
 
     def generate_video_from_image(self, image_path):
         self.log.info("Generating video from image")
-        with open(image_path, "rb") as input_image:
-            output = replicate.run(
-                "stability-ai/stable-video-diffusion:3f0457e4619daac51203dedb472816fd4af51f3149fa7a9e0b5ffcf1b8172438",
-                input={
-                    "cond_aug": 0.02,
-                    "decoding_t": 7,
-                    "input_image": input_image,
-                    "video_length": "14_frames_with_svd",
-                    "sizing_strategy": "maintain_aspect_ratio",
-                    "motion_bucket_id": 127,
-                    "frames_per_second": 6,
-                },
-            )
-            self.log.info(f"Generated video link: {output}")
-
-            return (output, output)
+        try:
+            with open(image_path, "rb") as input_image:
+                output = replicate.run(
+                    "stability-ai/stable-video-diffusion:3f0457e4619daac51203dedb472816fd4af51f3149fa7a9e0b5ffcf1b8172438",
+                    input={
+                        "cond_aug": 0.02,
+                        "decoding_t": 7,
+                        "input_image": input_image,
+                        "video_length": "14_frames_with_svd",
+                        "sizing_strategy": "maintain_aspect_ratio",
+                        "motion_bucket_id": 127,
+                        "frames_per_second": 6,
+                    },
+                )
+                self.log.info(f"Generated video link: {output}")
+                return (output, output)
+        except Exception as e:
+            self.log.error(f"Error: {e}")
+        return (None, "Unable to generate video from image")
